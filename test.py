2e69f35a085e66f354df65421299745a67561576
PauWol <145593735+PauWol@users.noreply.github.com>
2024-07-04 18:35:38 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,7 @@
 from ev3_bluetooth import EV3_Bluetooth
-import struct
-MACADDRESS = '00:16:53:48:08:C8'
+MACADDRESS = 'xx:xx:xx:xx:xx:xx'
 
 ev = EV3_Bluetooth(MACADDRESS)
-evstate = ev.Status()
 m = ev.Motor('a')
-#led = ev.Led('red','flash')
-
+led = ev.Led('red','flash')
 m.stop()
